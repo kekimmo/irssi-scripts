b43f1672cf8b5849960829cd0814ad6921e275ef
Kimmo Kenttälä <kimmo@kenttala.fi>
2013-05-11 23:37:21 +0300

    Initial commit

---
+++ urit.pl
@@ -0,0 +1,16 @@
+
+use strict;
+use warnings;
+
+use Irssi;
+
+our $VERSION = '0.01';
+our %IRSSI = (
+  authors     => 'Kimmo Kenttälä',
+  contact     => 'kimmo@kenttala.fi',
+  name        => 'Urit',
+  description => 'Urit interface',
+  license     => 'Undecided',
+);
+
+
